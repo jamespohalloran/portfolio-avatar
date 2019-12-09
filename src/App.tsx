b6f17e314593a6de0ebbd52a85bd8d38c8059c77
James O'Halloran <james.p.ohalloran@gmail.com>
2019-12-08 21:46:38 -0400

    format avatar

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,123 @@
 import React, { useEffect } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import "./avatar.css";
-import { TweenLite } from "gsap";
+import { TweenMax, TimelineMax } from "gsap";
+import { ReactComponent as Avatar } from "./avatar.svg";
 const App: React.FC = () => {
   useEffect(() => {
-    const myElement = document.getElementById("avatar") as any;
+    const face = document.getElementById("avatar") as any;
+    const hoof = document.getElementById("ClawHoof") as any;
+    const clawLeft = document.getElementById("ClawLeft") as any;
+    const clawTop = document.getElementById("ClawTop") as any;
+    const clawRight = document.getElementById("ClawRight") as any;
+    const eyeLeft = document.getElementById("EyeLeft") as any;
+    const eyeRight = document.getElementById("EyeRight") as any;
+    const mouth = document.getElementById("Mouth") as any;
+
     let rotation = 0;
-    setInterval(
-      () => {
-        if (Math.abs(rotation) <= 180) {
-          TweenLite.to(myElement, 1, {
-            rotation: rotation -= 90
-          });
-        }
+    let tl = new TimelineMax();
+    tl.to(
+      face,
+      1,
+      {
+        rotation: rotation -= 90
       },
-      1500,
-      500
+      "speech"
     );
+    tl.to(
+      hoof,
+      1,
+      {
+        // x: 110,
+        // y: 30,
+        borderRadius: "50%"
+      },
+      "speech"
+    );
+    // tl.to(
+    //   clawLeft,
+    //   1,
+    //   {
+    //     // x: 105,
+    //     // y: 30,
+    //     // height: 150,
+    //     width: 0
+    //   },
+    //   "speech"
+    // );
+    // tl.to(
+    //   clawRight,
+    //   1,
+    //   {
+    //     // x: 43,
+    //     // y: 10,
+    //     // height: 110,
+    //     width: 0
+    //   },
+    //   "speech"
+    // );
+    // tl.to(
+    //   clawTop,
+    //   1,
+    //   {
+    //     // x: 70,
+    //     // y: 140,
+    //     // height: 60,
+    //     width: 0
+    //   },
+    //   "speech"
+    // );
+    // tl.to(
+    //   mouth,
+    //   1,
+    //   {
+    //     // x: 100,
+    //     // y: -85,
+    //     // height: 110,
+    //     width: 0
+    //   },
+    //   "speech"
+    // );
+    // tl.to(
+    //   eyeLeft,
+    //   1,
+    //   {
+    //     // x: 100,
+    //     // y: -85,
+    //     // height: 110,
+    //     width: 0
+    //   },
+    //   "speech"
+    // );
+    // tl.to(
+    //   eyeRight,
+    //   1,
+    //   {
+    //     // x: 100,
+    //     // y: -85,
+    //     // height: 110,
+    //     width: 0
+    //   },
+    //   "speech"
+    // );
+
+    // setInterval(
+    //   () => {
+    //     if (Math.abs(rotation) <= 180) {
+    //       TweenMax.to(face, 1, {
+    //         rotation: rotation -= 90
+    //       });
+    //     }
+    //   },
+    //   1500,
+    //   500
+    // );
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
-        <svg
-          width="100%"
-          height="100%"
-          viewBox="0 0 251 251"
-          version="1.1"
-          xmlns="http://www.w3.org/2000/svg"
-          id="avatar"
-        >
-          <path
-            id="HeadOutline"
-            d="M187.853,1.5l-105.89,0l-38.113,38.753l0.672,46.601l-43.022,22.553l0.128,12.047l11.609,12.588l32.056,-1.647l-1.458,97.369l29.588,19.363l131.794,-1.554l43.171,-62.83l0.947,-120.74l-61.482,-62.503Z"
-          />
-          <rect id="Mouth" x="66.132" y="212.719" width="67.1" height="6.206" />
-          <rect
-            id="EyeLeft"
-            x="45.335"
-            y="149.382"
-            width="28.569"
-            height="29.163"
-          />
-          <rect
-            id="EyeRight"
-            x="133.526"
-            y="149.822"
-            width="29.648"
-            height="29.163"
-          />
-          <g id="RaptorLogo">
-            <rect x="107.522" y="48.038" width="26.85" height="30.021" />
-            <rect x="112.501" y="11.349" width="16.751" height="24.871" />
-            <rect x="140.514" y="21.023" width="8.608" height="20.983" />
-            <rect x="93.057" y="19.986" width="8.608" height="20.983" />
-          </g>
-        </svg>
+        <Avatar id="avatar" />
         <p>James Makes Stuff</p>
       </header>
     </div>
