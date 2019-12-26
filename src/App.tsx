2399e6350990b7705527a5229c35024e4a6785bd
James O'Halloran <james.p.ohalloran@gmail.com>
2019-12-26 11:55:04 -0400

    svg with multiple views

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,46 @@
 import React, { useEffect } from "react";
 import "./App.css";
 import "./avatar.css";
-import { TweenMax, TimelineMax } from "gsap";
-import { ReactComponent as Avatar } from "./avatar.svg";
+import { TweenMax, TweenLite, TimelineMax } from "gsap";
+import { ReactComponent as Avatar } from "./Self_multidimensional.svg";
+import { ReactComponent as SpeechBubble } from "./avatar.svg";
+import anime from "animejs";
+
 const App: React.FC = () => {
   useEffect(() => {
-    const face = document.getElementById("avatar") as any;
-    const hoof = document.getElementById("ClawHoof") as any;
-    const clawLeft = document.getElementById("ClawLeft") as any;
-    const clawTop = document.getElementById("ClawTop") as any;
-    const clawRight = document.getElementById("ClawRight") as any;
-    const eyeLeft = document.getElementById("EyeLeft") as any;
-    const eyeRight = document.getElementById("EyeRight") as any;
-    const mouth = document.getElementById("Mouth") as any;
+    const face = document.getElementsByClassName("avatar") as any;
+    const hoof = document.getElementsByClassName("ClawHoof") as any;
+    const clawLeft = document.getElementsByClassName("ClawLeft") as any;
+    const clawTop = document.getElementsByClassName("ClawTop") as any;
+    const clawRight = document.getElementsByClassName("ClawRight") as any;
+    const eyeLeft = document.getElementsByClassName("EyeLeft") as any;
+    const eyeRight = document.getElementsByClassName("EyeRight") as any;
+    const mouth = document.getElementsByClassName("Mouth") as any;
+
+    // M137,47c-5,-5 -25,-5 -30,0c-5,5 -5,25 0,30c5,5 25,5 30,0c5,-5 5,-25 0,-30Z
+    anime({
+      targets: [hoof],
+      d: "M137,47c-5,-5 -25,-5 -30,0l0,30l30,0l0,-30Z",
+      duration: 1000,
+      loop: true,
+      direction: "alternate",
+      easing: "linear"
+    });
+
+    // var hoofSnap = Snap.select("#ClawHoof");
+    // var hoofRoundSnap = Snap.select("#Round-ClawHoof");
+
+    // var hoofPoints = hoofSnap.node.getAttribute("d");
+    // var hoofRoundPoints = hoofSnap.node.getAttribute("d");
+
+    // hoofSnap.animate({ d: hoofRoundPoints }, 1000, mina.backout, () => {});
 
     let rotation = 0;
     let tl = new TimelineMax();
-    tl.to(
-      face,
-      1,
-      {
-        rotation: rotation -= 90
-      },
-      "speech"
-    );
+    // TweenLite.to("#HeadOutline", 1, {
+    //   morphSVG: "240,220 240,70 70,70 70,220"
+    // });
+
     tl.to(
       hoof,
       1,
@@ -101,23 +118,24 @@ const App: React.FC = () => {
     //   "speech"
     // );
 
-    // setInterval(
-    //   () => {
-    //     if (Math.abs(rotation) <= 180) {
-    //       TweenMax.to(face, 1, {
-    //         rotation: rotation -= 90
-    //       });
-    //     }
-    //   },
-    //   1500,
-    //   500
-    // );
+    setInterval(
+      () => {
+        if (Math.abs(rotation) <= 180) {
+          TweenMax.to(face, 1, {
+            rotation: rotation -= 90
+          });
+        }
+      },
+      1500,
+      500
+    );
   }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <Avatar id="avatar" />
+
         <p>James Makes Stuff</p>
       </header>
     </div>
