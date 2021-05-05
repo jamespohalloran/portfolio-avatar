2cb2ab46960b1343d613047c3c2f86351ce216ee
James O'Halloran <james.p.ohalloran@gmail.com>
2021-05-05 17:06:26 -0300

    add tina form

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,5 +1,24 @@
 import React from "react";
+import { useGraphqlForms } from "tina-graphql-gateway";
 
 export default function () {
+  const query = (gql: any) => gql`
+    query BlogPostQuery($relativePath: String!) {
+      getMarketingPagesDocument(relativePath: $relativePath) {
+        data {
+          __typename
+          ... on About_Doc_Data {
+            title
+            contactButton
+            _body
+          }
+        }
+      }
+    }
+  `;
+  const [payload, isLoading] = useGraphqlForms({
+    query,
+    variables: { relativePath: `about.md` },
+  });
   return <div>My admin page</div>;
 }
